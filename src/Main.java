3bbb2085eb38f904ef6dad9df3c703397719d022
Ирина Малова <irenespacenn@gmail.com>
2022-07-29 13:42:43 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int price = 8500;
+        int mile = 20;
+
+        int bonus = price / mile;
+        System.out.println("Бонус: " + bonus);
+
+    }
+
+}
